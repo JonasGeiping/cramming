2980a1dcb34f161a6c14bd09a84992c166d24da8
Jonas Geiping <jonas.geiping@googlemail.com>
2023-01-02 17:02:00 +0100

    remove superfluous lines from scripts

--- scripts/broad_ablation_c5_o3_a6000.sh
+++ scripts/broad_ablation_c5_o3_a6000.sh
@@ -24,8 +24,8 @@ python eval.py eval=GLUE_sane name=A6000amp_b4096_c5_o0_final wandb.tags=[cc,c5]
 
 # # basic architecture
 # # crashes occasionally with nonfinite loss
-python pretrain.py name=A6000amp_b4096_orig_o3_final wandb.tags=[o3,cc] base_dir=/cmlscratch/jonas0/cramming/ impl.local_staging_dir=/scratch1/jonas0 arch=bert-original train=bert-o3 train.batch_size=4096 data=c4-subset data.remove_trash=True data.trash_cutoff=0.25 data.ordering=sentence-length-curriculum data.deduplicate_entries=True data.deduplication_threshold=75 data.max_seq_in_tokenized_dataset=85e6
-python eval.py eval=GLUE_sane name=A6000amp_b4096_orig_o3_final base_dir=/cmlscratch/jonas0/cramming/ wandb.tags=[cc,o3] eval.checkpoint=latest impl.microbatch_size=16 impl.shuffle_in_dataloader=True eval.scheduler=cosine-decay eval.epochs=5 eval.batch_size=16 eval.optim.lr=4e-5
+python pretrain.py name=A6000amp_b4096_orig_o3_final wandb.tags=[o3,cc] arch=bert-original train=bert-o3 train.batch_size=4096 data=c4-subset data.remove_trash=True data.trash_cutoff=0.25 data.ordering=sentence-length-curriculum data.deduplicate_entries=True data.deduplication_threshold=75 data.max_seq_in_tokenized_dataset=85e6
+python eval.py eval=GLUE_sane name=A6000amp_b4096_orig_o3_final wandb.tags=[cc,o3] eval.checkpoint=latest impl.microbatch_size=16 impl.shuffle_in_dataloader=True eval.scheduler=cosine-decay eval.epochs=5 eval.batch_size=16 eval.optim.lr=4e-5
 
 
 # # basic architecture + simple training routine
